dd263fce3af711ab3d2f443b0acb0aae381d7ba8
Karl Giesing <karl.giesing001@umb.edu>
2014-12-19 22:29:39 -0500

    Changes to main activity for testing
    Added a SeekBar to allow testing of programmatic changes to Knob level,
    and TextView to view current (SeekBar or Knob) level.

--- src/giesing/karl/android/widgets/MainActivity.java
+++ src/giesing/karl/android/widgets/MainActivity.java
@@ -5,34 +5,58 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	Knob widget;
+	SeekBar seekBar;
+	TextView tvProgress;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
+		tvProgress = (TextView) findViewById(R.id.tvProgress);
+		seekBar = (SeekBar) findViewById(R.id.seekBar1);
+		seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// no-op
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// no-op
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				// Set progress in knob
+				widget.setLevel(progress);
+				tvProgress.setText("progress: " + progress);
+			}
+		});
 		widget = (Knob) findViewById(R.id.widget);
 		widget.setOnKnobChangeListener(new Knob.OnKnobChangeListener() {
 			
 			@Override
 			public void onStopTrackingTouch(Knob knob) {
-				// TODO Auto-generated method stub
+				// no-op
 			}
 			
 			@Override
 			public void onStartTrackingTouch(Knob knob) {
-				// TODO Auto-generated method stub
-				
+				// no-op
 			}
 			
 			@Override
-			public void onLevelChanged(Knob knob, float progress,
+			public void onLevelChanged(Knob knob, float level,
 					boolean fromUser) {
-				// TODO Auto-generated method stub
-				
+				seekBar.setProgress((int) level);
+				tvProgress.setText("level: " + level);
 			}
 		});
 	}
